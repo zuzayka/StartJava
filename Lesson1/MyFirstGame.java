c35da545aebbe2916da313e524b9642b9b4e5c89
zuzayka <bool6@yandex.ru>
2021-11-08 15:05:49 +0300

    изменил MyFirstGame

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -1,47 +1,29 @@
 public class MyFirstGame {
     public static void main(String[] args) {
         int pcChoice = (int) (Math.random() * 101);
-        System.out.println("pc think number: " + pcChoice);
+        System.out.println("pc think number: " + pcChoice + "\n");
         System.out.println(findNum(pcChoice));
     }
 
     public static int findNum(int askNum) {
-        int[] temp = new int[100];
-        int i = 1;
-        int limit = 100;
-        temp[0] = limit / 2;
-        limit /= 4;
-        if (temp[0] > askNum) {
-            temp[1] = temp[0] - limit;
-        } else if (temp[0] < askNum) {
-            temp[1] = temp[0] + limit;
-        } else {
-            return temp[0];
-        }
-        limit = 32;
-        while (temp[i] != askNum) {
-            if (limit > 1) {
-                if (limit % 2 != 0) {
-                    limit = (limit + 1) / 2;
+        int num = 50;
+        int stepNum = 64;
+        while (num != askNum) {
+            System.out.println(num);
+            stepNum /= 2;
+            if (num < askNum) {
+                num += stepNum;
+                if (num > 100) {
+                    num = 100;
                 }
-                limit /= 2;
+                continue;
+            } else if (num > askNum) {
+                num -= stepNum;
+                if (num < 0) {
+                    num = 0;
+                }
             }
-            if (((temp[i] < askNum) && (temp[i] < temp[i - 1])) || ((temp[i] < askNum) && temp[i] > temp[i - 1])) {
-                temp[i + 1] = temp[i] + limit;
-                if (temp[i] + limit > 100) {
-                    temp[i + 1] = 100;
-                }
-            } else if (((temp[i] > askNum) && (temp[i] > temp[i - 1]) || (temp[i] > askNum) && temp[i] < temp[i - 1])) {
-                temp[i + 1] = temp[i] - limit;
-                if (temp[i] - limit < 0) {
-                    temp[i + 1] = 0;
-                }
-            } else if (temp[i] == askNum) {
-                return temp[i];
-            }
-            System.out.println(temp[i]);
-            i++;
         }
-        return temp[i];
+        return num;
     }
 }
