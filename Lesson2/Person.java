4ae2944697a7fd4d2130dc8f5c7311b63c44c4e8
zuzayka <bool6@yandex.ru>
2021-11-16 13:44:18 +0300

    добавил Peson

---
+++ Lesson2/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+    String sex = "male";
+    String name = "John";
+    float height = 1.71;
+    float weight = 82.3;
+    int age = 81;
+    void go() {
+        System.out.println("going");
+    }
+    void sit() {
+        System.out.println("sitting");
+    }
+    void - run() {
+        System.out.println("running");
+    }
+    void say() {
+        System.out.println("Hi there!");
+    }
+    void learnJava {
+        System.out.println("learning Java");
+    }
+}
