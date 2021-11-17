d411d758b776c18cfff849cf332a024d61dceb71
zuzayka <67962969+zuzayka@users.noreply.github.com>
2021-11-17 09:08:58 +0300

    Update Person.java

--- Lesson2/Person.java
+++ Lesson2/Person.java
@@ -1,21 +1,26 @@
 public class Person {
     String sex = "male";
     String name = "John";
-    float height = 1.71;
-    float weight = 82.3;
+    float height = 1.71f;
+    float weight = 82.3f;
     int age = 81;
+
     void go() {
         System.out.println("going");
     }
+
     void sit() {
         System.out.println("sitting");
     }
-    void - run() {
+
+    void run() {
         System.out.println("running");
     }
+
     void say() {
         System.out.println("Hi there!");
     }
+
     void learnJava {
         System.out.println("learning Java");
     }
