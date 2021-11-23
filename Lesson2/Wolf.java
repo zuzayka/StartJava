fc1a0ca3f1fec5b954167c2722335d5b10eca931
zuzayka <bool6@yandex.ru>
2021-11-23 11:38:25 +0300

    исправил 4-е дз Wolf, WolfTest

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -53,19 +53,19 @@ class Wolf {
         return age;
     }
 
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
     public void setAge(int age) {
         if (age < 0 || age > 8) {
             System.out.println("Некорректный возраст");
         } else {
             this.age = age;
         }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 }
